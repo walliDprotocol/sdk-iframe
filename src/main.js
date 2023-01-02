58883992e07a018bdbfad44b9c052890cd769896
Vitor Viana <masterviana@gmail.com>
2023-01-02 09:54:28 +0000

    add pubsub

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import PubNubVue from "pubnub-vue";
 import vuetify from "./plugins/vuetify";
 import "@babel/polyfill";
 
 import "./assets/css/vars.scss";
 import "./assets/css/main.scss";
+
+// https://www.pubnub.com/docs/sdks/vue
+Vue.use(PubNubVue, {
+  subscribeKey: "YOUR SUBSCRIBE KEY HERE",
+  publishKey: "YOUR PUBLISH KEY HERE",
+});
 
 Vue.config.productionTip = false;
 
