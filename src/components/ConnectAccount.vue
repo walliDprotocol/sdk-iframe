5d6e8135eda46dd1e69f6d28d0385a4ad3705ccf
GuilhermeArsenio <78441164+GuilhermeArsenio@users.noreply.github.com>
2023-02-22 21:50:29 +0000

    fix message css

--- src/components/ConnectAccount.vue
+++ src/components/ConnectAccount.vue
@@ -1,95 +1,100 @@
 <template>
-  <v-container class="connect-account pa-3">
-    <v-row class="text-center">
-      <v-col cols="12" class="d-flex">
-        <v-col cols="auto" class="d-flex align-center">
-          <v-img
-            :src="
-              selectedAccount.imgType === 'url'
-                ? selectedAccount.imgUrl
-                : `./logos/${selectedAccount.IdName}.webp`
-            "
-            contain
-            max-height="40"
-            max-width="40"
-          />
-        </v-col>
-        <v-col
-          cols="8"
-          class="pl-0 d-flex flex-column align-start justify-center"
-        >
-          <p class="bold-text-p text-uppercase mb-0">
-            {{ selectedAccount.IdNameDesc }}
-          </p>
-          <p class="mb-0">
-            {{ userData.username }}
-          </p>
-          <p class="bold-text-p mb-0">
-            {{ userData.name }}
-          </p>
-        </v-col>
-        <v-spacer />
-
-        <v-col cols="2" class="d-flex flex-column align-center justify-center">
-          <p
-            class="bold-text-p text-uppercase mb-0"
-            :style="{
-              cursor: 'pointer',
-              color: allSelected ? '#01022E' : '#00AFC5',
-            }"
-            @click="setAll"
+  <div>
+    <v-container class="connect-account pa-3">
+      <v-row class="text-center">
+        <v-col cols="12" class="d-flex">
+          <v-col cols="auto" class="d-flex align-center">
+            <v-img
+              :src="
+                selectedAccount.imgType === 'url'
+                  ? selectedAccount.imgUrl
+                  : `./logos/${selectedAccount.IdName}.webp`
+              "
+              contain
+              max-height="40"
+              max-width="40"
+            />
+          </v-col>
+          <v-col
+            cols="8"
+            class="pl-0 d-flex flex-column align-start justify-center"
           >
-            {{ allSelected ? "Selected!" : "Select all" }}
-          </p>
-        </v-col>
-      </v-col>
-    </v-row>
+            <p class="bold-text-p text-uppercase mb-0">
+              {{ selectedAccount.IdNameDesc }}
+            </p>
+            <p class="mb-0">
+              {{ userData.username }}
+            </p>
+            <p class="bold-text-p mb-0">
+              {{ userData.name }}
+            </p>
+          </v-col>
+          <v-spacer />
 
-    <v-row
-      class="text-center"
-      v-for="(item, index) in selectedAccount.options"
-      :key="index"
-    >
-      <v-col cols="12" class="d-flex align-start">
-        <v-col cols="2" class="d-flex fill-height align-start">
-          <p class="bold-text-p text-left mb-0">
-            {{ item.field }}
-          </p>
-        </v-col>
-        <v-col
-          cols="5"
-          class="pl-0 d-flex fill-height flex-column align-start justify-space-between"
-        >
-          <p
-            class="normal-text-p text-left"
-            v-for="(desc, index) in item.description"
-            :key="index"
+          <v-col
+            cols="2"
+            class="d-flex flex-column align-center justify-center"
           >
-            {{ desc }}
-          </p>
+            <p
+              class="bold-text-p text-uppercase mb-0"
+              :style="{
+                cursor: 'pointer',
+                color: allSelected ? '#01022E' : '#00AFC5',
+              }"
+              @click="setAll"
+            >
+              {{ allSelected ? "Selected!" : "Select all" }}
+            </p>
+          </v-col>
         </v-col>
-        <v-spacer />
-        <v-col
-          cols="2"
-          class="d-flex fill-height flex-column align-center justify-center py-4"
-        >
-          <v-switch
-            class="mt-0 form-switch"
-            v-model="item.state"
-            hide-details
-            :ripple="false"
-          ></v-switch>
+      </v-row>
+
+      <v-row
+        class="text-center"
+        v-for="(item, index) in selectedAccount.options"
+        :key="index"
+      >
+        <v-col cols="12" class="d-flex align-start">
+          <v-col cols="2" class="d-flex fill-height align-start">
+            <p class="bold-text-p text-left mb-0">
+              {{ item.field }}
+            </p>
+          </v-col>
+          <v-col
+            cols="5"
+            class="pl-0 d-flex fill-height flex-column align-start justify-space-between"
+          >
+            <p
+              class="normal-text-p text-left"
+              v-for="(desc, index) in item.description"
+              :key="index"
+            >
+              {{ desc }}
+            </p>
+          </v-col>
+          <v-spacer />
+          <v-col
+            cols="2"
+            class="d-flex fill-height flex-column align-center justify-center py-4"
+          >
+            <v-switch
+              class="mt-0 form-switch"
+              v-model="item.state"
+              hide-details
+              :ripple="false"
+            ></v-switch>
+          </v-col>
         </v-col>
-      </v-col>
-    </v-row>
-    <v-row v-if="errorMessage">
-      <v-col>
+      </v-row>
+    </v-container>
+    <v-row class="message-row" v-if="errorMessage">
+      <v-col class="pa-5 py-6">
         <p :class="{ 'error-message': errorMessage }">
           {{ errorMessage }}
         </p>
       </v-col>
     </v-row>
-  </v-container>
+  </div>
 </template>
 
 <script>
@@ -152,6 +157,13 @@ export default {
 </script>
 <style lang="scss">
 .error-message {
-  color: red;
+  font-family: Karla;
+  font-size: 13px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.51;
+  letter-spacing: normal;
+  color: #f02049;
 }
 </style>
