14c35ce1d4c010f8c38e2b87a2ce0d604ce8e13c
GuilhermeArsenio <78441164+GuilhermeArsenio@users.noreply.github.com>
2023-05-31 17:03:18 +0100

    ui changes to wallet selector

--- src/components/WalletSelector/WalletSelector.vue
+++ src/components/WalletSelector/WalletSelector.vue
@@ -1,21 +1,20 @@
 <template>
-  <v-container class="connect-account">
-    <v-row>
-      <v-col cols="4" class="pa-5">
+  <v-container class="connect-account wallet-selector">
+    <v-row justify="space-between">
+      <v-col cols="4" class="pa-5 pr-1 pb-0">
         <h4 style="font-weight: 600">{{ $t(`walletSelector.label`) }}</h4>
-        <v-radio-group v-model="value" row class="mt-0 pt-0 wallet-radio">
+        <v-radio-group v-model="value" row class="mt-3 wallet-radio">
           <v-radio
-            v-for="{ name } in wallets"
-            :key="name"
-            :value="name"
-            class="mr-12"
+            v-for="{ id, name } in wallets"
+            :key="id"
+            :value="id"
             color="#009fb1"
             :ripple="false"
           >
             <template v-slot:label>
-              <div class="d-flex align-center wallet-radio-button" style="font-size: 14px">
+              <div class="d-flex align-center wallet-radio-button pa-2" style="font-size: 14px">
                 <div class="d-flex align-center justify-center mr-3 wallet-radio-icon">
-                  <img :src="require(`./logos/${name}.webp`)" alt="" width="20px" height="20px" />
+                  <img :src="require(`./logos/${id}.webp`)" alt="" width="38px" height="38px" />
                 </div>
                 {{ name }}
               </div>
@@ -23,7 +22,7 @@
           </v-radio>
         </v-radio-group>
       </v-col>
-      <v-col cols="8" class="pa-16">
+      <v-col cols="10" sm="7" class="pa-16">
         <h4 class="text-center" style="font-weight: 600">{{ $t(`walletSelector.text`) }}</h4>
       </v-col>
     </v-row>
@@ -37,7 +36,24 @@ export default {
     return {
       wallets: [
         {
-          name: "metamask",
+          id: "metamask",
+          name: "Metamask",
+        },
+        {
+          id: "valora",
+          name: "Valora",
+        },
+        {
+          id: "libera",
+          name: "Libera",
+        },
+        {
+          id: "celo-wallet",
+          name: "Celo Extension Wallet",
+        },
+        {
+          id: "wallet-connect",
+          name: "WalletConnect",
         },
       ],
     };
@@ -58,11 +74,36 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
-.connect-account {
+<style lang="scss">
+.connect-account.wallet-selector {
   .row {
     .col:first-child {
       border-right: solid 1px #eee;
+    }
+  }
+  .wallet-radio {
+    max-height: 145px;
+    overflow-y: auto;
+    .v-input--selection-controls__input {
+      display: none;
+    }
+    .v-radio {
+      border-radius: 8px;
+      width: 100%;
+      margin-bottom: 2px;
+    }
+    .v-item--active {
+      background-color: #f4f4f4;
+    }
+    .wallet-radio-button {
+      font-family: Karla;
+      font-size: 15px;
+      font-weight: normal;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: 1.8;
+      letter-spacing: normal;
+      color: #01022e;
     }
   }
 }
